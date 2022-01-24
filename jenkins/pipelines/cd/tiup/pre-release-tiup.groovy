3718c1ee9c056bc51b9c02e05879ede31daf0512
Qiang Zhou <zhouqiang.cl@gmail.com>
2022-01-24 23:32:46 +0800

    trigger pre online docker
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- jenkins/pipelines/cd/tiup/pre-release-tiup.groovy
+++ jenkins/pipelines/cd/tiup/pre-release-tiup.groovy
@@ -172,27 +172,43 @@ stage('Build') {
     parallel builds
 }
 
-stage('releaese tiup') {
-    build job: "tiup-mirror-update-test-hotfix",
-        wait: true,
-        parameters: [
-                [$class: 'StringParameterValue', name: 'TIDB_TAG', value: tidb_sha1],
-                [$class: 'StringParameterValue', name: 'TIKV_TAG', value: tikv_sha1],
-                [$class: 'StringParameterValue', name: 'PD_TAG', value: pd_sha1],
-                [$class: 'StringParameterValue', name: 'BINLOG_TAG', value: binlog_sha1],
-                [$class: 'StringParameterValue', name: 'IMPORTER_TAG', value: importer_sha1],
-                [$class: 'StringParameterValue', name: 'CDC_TAG', value: cdc_sha1],
-                [$class: 'StringParameterValue', name: 'BR_TAG', value: br_sha1],
-                [$class: 'StringParameterValue', name: 'DUMPLING_TAG', value: dumpling_sha1],
-                [$class: 'StringParameterValue', name: 'TIFLASH_TAG', value: tiflash_sha1],
-                [$class: 'StringParameterValue', name: 'HOTFIX_TAG', value: RELEASE_TAG],
-                [$class: 'StringParameterValue', name: 'TIDB_CTL_TAG', value: tidb_ctl_githash],
-                [$class: 'StringParameterValue', name: 'TIUP_MIRRORS', value: TIUP_MIRRORS],
-                [$class: 'BooleanParameterValue', name: 'ARCH_ARM', value: ARCH_ARM],
-                [$class: 'BooleanParameterValue', name: 'ARCH_X86', value: ARCH_X86],
-                [$class: 'BooleanParameterValue', name: 'ARCH_MAC', value: ARCH_MAC],
-                [$class: 'BooleanParameterValue', name: 'ARCH_MAC_ARM', value: ARCH_MAC_ARM],
+stage('pre-release') {
+    release = [:]
+    release["Pre release TiUP"] = {
+        build job: "tiup-mirror-update-test-hotfix",
+            wait: true,
+            parameters: [
+                    [$class: 'StringParameterValue', name: 'TIDB_TAG', value: tidb_sha1],
+                    [$class: 'StringParameterValue', name: 'TIKV_TAG', value: tikv_sha1],
+                    [$class: 'StringParameterValue', name: 'PD_TAG', value: pd_sha1],
+                    [$class: 'StringParameterValue', name: 'BINLOG_TAG', value: binlog_sha1],
+                    [$class: 'StringParameterValue', name: 'IMPORTER_TAG', value: importer_sha1],
+                    [$class: 'StringParameterValue', name: 'CDC_TAG', value: cdc_sha1],
+                    [$class: 'StringParameterValue', name: 'BR_TAG', value: br_sha1],
+                    [$class: 'StringParameterValue', name: 'DUMPLING_TAG', value: dumpling_sha1],
+                    [$class: 'StringParameterValue', name: 'TIFLASH_TAG', value: tiflash_sha1],
+                    [$class: 'StringParameterValue', name: 'HOTFIX_TAG', value: RELEASE_TAG],
+                    [$class: 'StringParameterValue', name: 'TIDB_CTL_TAG', value: tidb_ctl_githash],
+                    [$class: 'StringParameterValue', name: 'TIUP_MIRRORS', value: TIUP_MIRRORS],
+                    [$class: 'BooleanParameterValue', name: 'ARCH_ARM', value: ARCH_ARM],
+                    [$class: 'BooleanParameterValue', name: 'ARCH_X86', value: ARCH_X86],
+                    [$class: 'BooleanParameterValue', name: 'ARCH_MAC', value: ARCH_MAC],
+                    [$class: 'BooleanParameterValue', name: 'ARCH_MAC_ARM', value: ARCH_MAC_ARM],
+                    [$class: 'StringParameterValue', name: 'RELEASE_BRANCH', value: RELEASE_BRANCH],
+            ]
+    }
+    release["Pre release Docker Image"] = {
+        build job: "pre-release-docker",
+            wait: true,
+            parameters: [
                 [$class: 'StringParameterValue', name: 'RELEASE_BRANCH', value: RELEASE_BRANCH],
-        ]
+                [$class: 'StringParameterValue', name: 'RELEASE_TAG', value: RELEASE_TAG],
+                [$class: 'StringParameterValue', name: 'TIKV_BUMPVERION_HASH', value: tikv_sha1],
+                [$class: 'StringParameterValue', name: 'TIKV_BUMPVERSION_PRID', value: TIKV_BUMPVERSION_PRID],
+                [$class: 'BooleanParameterValue', name: 'FORCE_REBUILD', value: false],
+            ]
+    }
+
+    parallel release
 
 }
