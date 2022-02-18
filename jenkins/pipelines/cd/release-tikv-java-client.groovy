5db4b6d9287a2258ebac4efaa819679324e2d435
VelocityLight <yjchit@126.com>
2022-02-18 16:11:27 +0800

    fixme: import not use option

--- jenkins/pipelines/cd/release-tikv-java-client.groovy
+++ jenkins/pipelines/cd/release-tikv-java-client.groovy
@@ -49,10 +49,8 @@ pipeline {
                     sh '''
                         curl http://fileserver.pingcap.net/download/gpgkey_pub.gpg -o gpgkey_pub.gpg
                         curl http://fileserver.pingcap.net/download/gpgkey_secret.gpg -o gpgkey_secret.gpg
-                        export GPG_TTY=$(tty)
-                        ls
                         gpg --import gpgkey_pub.gpg
-                        gpg --allow-secret-key-import --import gpgkey_secret.gpg
+                        gpg --import gpgkey_secret.gpg
                         gpg --list-keys
                     '''
                 }
